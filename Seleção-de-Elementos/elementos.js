13fc523c270a448513b77a32a5c847eac8fcc274
AndreJorge01 <andrejorge386@gmail.com>
2025-07-24 23:18:47 +0100

    exercicio

--- Seleção-de-Elementos/elementos.js
+++ Seleção-de-Elementos/elementos.js
@@ -2,11 +2,11 @@
 //getElementById seleciona e retorna elementos do DOM
 
 // Seleciona pelo ID
-const animaisSection = document.getElementById('animais');
-const contatoSection = document.getElementById('contato');
+const animaisSection = document.getElementById("animais");
+const contatoSection = document.getElementById("contato");
 
 // Retorna null caso não exista
-const naoExiste = document.getElementById('teste');
+const naoExiste = document.getElementById("teste");
 
 //Classe e Tag
 //getElementsByClassName e getElementsByTagName selecionam e retornam uma lista de elementos do DOM. A lista retornada está ao vivo, significa que se elementos forem adicionados, ela será automaticamente atualizada.
@@ -16,31 +16,30 @@ const naoExiste = document.getElementById('teste');
 //const contato = document.getElementsByClassName('grid-section contato');
 
 // Seleciona todas as UL's, retorna uma HTMLCollection
-const ul = document.getElementsByTagName('ul');
+const ul = document.getElementsByTagName("ul");
 
 // Retorna o primeiro elemento
 console.log(gridSection[0]);
 
-
 //Seletor Geral Único
 //querySelector retorna o primeiro elemento que combinar com o seu seletor CSS.
 
-const animais = document.querySelector('.animais');
-const contato = document.querySelector('#contato');
-const ultimoItem = document.querySelector('.animais-lista li:last-child');
+const animais = document.querySelector(".animais");
+const contato = document.querySelector("#contato");
+const ultimoItem = document.querySelector(".animais-lista li:last-child");
 const linkCSS = document.querySelector('[href^="https://"]');
-const primeiroUl = document.querySelector('ul');
+const primeiroUl = document.querySelector("ul");
 
 // Busca dentro do Ul apenas
-const navItem = primeiroUl.querySelector('li');
+const navItem = primeiroUl.querySelector("li");
 
 //Seletor Geral Lista
 //querySelectorAll retorna todos os elementos compatíveis com o seletor CSS em uma NodeList
 
-const gridSection = document.querySelectorAll('.grid-section');
-const listas = document.querySelectorAll('ul');
-const titulos = document.querySelectorAll('.titulo');
-const fotosAnimais = document.querySelectorAll('.animais-lista img');
+const gridSection = document.querySelectorAll(".grid-section");
+const listas = document.querySelectorAll("ul");
+const titulos = document.querySelectorAll(".titulo");
+const fotosAnimais = document.querySelectorAll(".animais-lista img");
 
 // Retorna o segundo elemento
 console.log(gridSection[1]);
@@ -50,11 +49,11 @@ console.log(gridSection[1]);
 //HTMLCollection vs NodeList
 //A diferença está nos métodos e propriedades de ambas. Além disso a NodeList retornada com querySelectorAll é estática.
 
-const titulo = document.querySelector('.titulo');
-const gridSectionHTML = document.getElementsByClassName('grid-section');
-const gridSectionNode = document.querySelectorAll('.grid-section');
+const titulo = document.querySelector(".titulo");
+const gridSectionHTML = document.getElementsByClassName("grid-section");
+const gridSectionNode = document.querySelectorAll(".grid-section");
 
-titulo.classList.add('grid-section');
+titulo.classList.add("grid-section");
 
 console.log(gridSectionHTML); // 4 itens
 console.log(gridSectionNode); // 3 itens
@@ -65,13 +64,23 @@ console.log(gridSectionNode); // 3 itens
 //É possível transformar array-like em uma Array real, utilizando o método Array.from(gridSection)
 
 // Retorne no console todas as imagens do site
-
+const img = document.querySelectorAll("img");
+console.log(img);
 // Retorne no console apenas as imagens que começaram com a palavra imagem
-
+const imgName = document.querySelectorAll('img[src^="img/imagem"]');
+console.log(imgName);
 // Selecione todos os links internos (onde o href começa com #)
-
+const linksInterno = document.querySelectorAll('[href^="#"]');
+console.log(linksInterno);
 // Selecione o primeiro h2 dentro de .animais-descricao
-
+const primeiroH1 = document.querySelector(".animais.descricao h2");
+//ou
+const h2Animais = animais.querySelector("h2");
+console.log(primeiroH1);
+console.log(h2Animais);
 // Selecione o último p do site
+const paragrafos = document.querySelectorAll("p");
 
-
+console.log(paragrafos[paragrafos.length - 1]);
+//ou
+console.log(paragrafos[--paragrafos.length]);
