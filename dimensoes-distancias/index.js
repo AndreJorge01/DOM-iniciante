df8e84114c1f00b7f2e32375e3a2efec954cff08
AndreJorge01 <andrejorge386@gmail.com>
2025-08-24 15:00:15 +0100

    exercicio

--- dimensoes-distancias/index.js
+++ dimensoes-distancias/index.js
@@ -68,12 +68,45 @@ if (small.matches) {
 
 // Verifique a distância da primeira imagem
 // em relação ao topo da página
+const img = document.querySelector("img");
+const imgTop = img.offsetHeight;
 
+console.log(imgTop);
 // Retorne a soma da largura de todas as imagens
+function somaImagens() {
+  const imagens = document.querySelectorAll("img");
+  soma = 0;
+  imagens.forEach((imagem) => {
+    soma = soma + imagem.offsetHeight;
+  });
+  console.log(soma);
+}
 
+window.onload = function () {
+  somaImagens();
+};
 // Verifique se os links da página possuem
 // o mínimo recomendado para telas utilizadas
 // com o dedo. (48px/48px de acordo com o google)
 
+const links = document.querySelectorAll("a");
+
+links.forEach((link) => {
+  const widthLink = link.offsetWidth;
+  const heightLink = link.offsetHeight;
+  if (widthLink >= 48 && heightLink >= 48) {
+    console.log(link, "Possui boa acessibilidade");
+  } else {
+    console.log(link, " Nao possui boa acessibilidade");
+  }
+});
+
 // Se o browser for menor que 720px,
 // adicione a classe menu-mobile ao menu
+
+const browserSmall = window.matchMedia("(max-width: 720px)").matches;
+
+if (browserSmall) {
+  const menu = document.querySelectorAll(".menu");
+  menu.classList.add("menu-mobile");
+}
