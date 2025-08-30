2e619f611ae77903404e23e8806f319c6042076f
AndreJorge01 <andrejorge386@gmail.com>
2025-08-30 21:45:28 +0100

    exercicio

--- exercicios/index.js
+++ exercicios/index.js
@@ -1,10 +1,14 @@
 // exemplo de codigo de loop de incrementacao
+// for = para quanto eu sei exatamento o inicio o meio e fim do loop
 
 function loop() {
   for (let i = 0; i <= 10; i++) {
     console.log(i);
   }
 }
+
+// while = Usado quando não sabemos exatamente quantas vezes vamos repetir.
+//Continua rodando enquanto a condição for verdadeira.
 
 let lista = 0;
 
@@ -15,3 +19,33 @@ function loopLista() {
   }
 }
 
+// navegacao por tabs
+
+// selecionar os elementos a ser alterado
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+  tabContent[0].classList.add("ativo");
+
+  // funcao para adicionar a classe ativo para cada section
+  if (tabMenu.length && tabContent.length) {
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
+
+    // funcao para ativar a funcao activetab ao click
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+initTabNav();
+
+
+// accordion list
