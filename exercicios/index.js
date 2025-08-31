d742d845f127875e355da3b5baab13a8594b4130
AndreJorge01 <andrejorge386@gmail.com>
2025-08-31 13:11:13 +0100

    accordionlist

--- exercicios/index.js
+++ exercicios/index.js
@@ -47,5 +47,24 @@ function initTabNav() {
 }
 initTabNav();
 
-
 // accordion list
+function initAccordionList () {
+ const accordionList = document.querySelectorAll(".js-accordion dt");
+const activeClass = 'ativo'
+
+if(accordionList.length) {
+ accordionList[0].classList.add(activeClass);
+accordionList[0].nextElementSibling.classList.add(activeClass);
+
+function activeAccordion() {
+  this.classList.toggle(activeClass);
+  this.nextElementSibling.classList.toggle(activeClass);
+}
+
+accordionList.forEach((item) => {
+  item.addEventListener("click", activeAccordion);
+});
+}
+}
+
+initAccordionList()
