38740739b970e21942f85e0bf51aa812bf72d2a5
AndreJorge01 <andrejorge386@gmail.com>
2025-08-31 13:58:46 +0100

    animacaoScroll

--- exercicios/index.js
+++ exercicios/index.js
@@ -97,3 +97,29 @@ function initScrollSuave() {
   });
 }
 initScrollSuave();
+
+// animacao ao scroll
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll("js-scroll");
+
+  if (section.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+initAnimacaoScroll();
